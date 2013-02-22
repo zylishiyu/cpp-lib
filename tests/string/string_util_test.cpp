d6db6896fc58f949383ae69e61acb1852aba4075
lishiyu <lishiyu@baidu.com>
2013-02-23 01:14:15 +0800

    update string util test case ;

--- tests/string/string_util_test.cpp
+++ tests/string/string_util_test.cpp
@@ -3,13 +3,13 @@
 #include "string_util.h"
 
 void strrstr_test() {
-    char str[1024] = "/baidu/ns/ksarch/cache-test/product/shiyutest/server_list1/s00001";
-    printf("%s\n", strrstr("/baidu/ns/ksarch/cache-test/product/shiyutest/server_list1/s00001", "product"));
-    printf("%s\n", strrstr("/baidu/ns/ksarch/cache-test/product/shiyutest/server_list1/s00001", "server_list"));
+    char str[1024] = "/app/cache-test/product/test/server_list1/s00001";
+    printf("%s\n", strrstr("/app/cache-test/product/test/server_list1/s00001", "product"));
+    printf("%s\n", strrstr("/app/cache-test/product/test/server_list1/s00001", "server_list"));
     char* pos = (char*)strrstr(str, "/s0");
     *pos = '\0';
     printf("=============%s\n", str);
-    printf("%s\n", strrstr("/baidu/ns/ksarch/cache-test/product/shiyutest/server_list1/s00001", "test"));
+    printf("%s\n", strrstr("/app/cache-test/product/test/server_list1/s00001", "test"));
 }
 
 int main() {
