43fc2b33dfe31992851c7afab2a25fa8afbece73
lishiyu <lishiyu@baidu.com>
2013-02-23 01:09:43 +0800

    add daemonize lib

---
+++ lib_src/process/daemonize.h
@@ -0,0 +1,12 @@
+#ifndef  __ZYLISHIYU_DAEMONIZE_H_
+#define  __ZYLISHIYU_DAEMONIZE_H_
+
+
+void daemonize(const char* cmd);
+
+int already_running(void);
+
+
+
+#endif  // __ZYLISHIYU_DAEMONIZE_H_
+
---
+++ lib_src/process/proc_util.h
@@ -0,0 +1,9 @@
+#ifndef  __ZYLISHIYU_PROC_UTIL_H_
+#define  __ZYLISHIYU_PROC_UTIL_H_
+
+#include "daemonize.h"
+#include "proc_sync.h"
+
+
+#endif  // __ZYLISHIYU_PROC_UTIL_H_
+
